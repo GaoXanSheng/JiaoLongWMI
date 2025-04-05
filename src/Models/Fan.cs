10fac5ca844b84e7209c47add1e1653d9bd9c749
GaoXuSheng <moxiaojulove@foxmail.com>
2025-04-05 21:22:04 +0800

    fix: 修复风扇转速设置错误
    在ECMemoryTable.cs中，将Fan_RPM_SET拆分为Fan1_RPM_SET和Fan2_RPM_SET，分别对应0xC83C和0xC83D。在Fan.cs中，移除Fan_RPM_SET的写入，添加Fan1_RPM_SET和Fan2_RPM_SET的写入，并更新0xB20寄存器以启用风扇2。

--- src/Models/Fan.cs
+++ src/Models/Fan.cs
@@ -19,10 +19,14 @@ public class Fan
         {
             return librarySutatus;
         }
-
-        winRing0.ECRamWriteExt_Direct((ushort)ECMemoryTable.Fan_RPM_SET, outSpeed);
+        winRing0.ECRamWriteExt_Direct((ushort)ECMemoryTable.Fan1_RPM_SET, outSpeed);
         byte mask = winRing0.ECRamReadExt_Direct(0xB20);
         mask |= 0x02;
+        winRing0.ECRamWriteExt_Direct(0xB20, mask);
+
+        winRing0.ECRamWriteExt_Direct((ushort)ECMemoryTable.Fan2_RPM_SET, outSpeed);
+        mask = winRing0.ECRamReadExt_Direct(0xB20);
+        mask |= 0x08;
         winRing0.ECRamWriteExt_Direct(0xB20, mask);
 
         winRing0.Dispose();
--- src/tools/ECMemoryTable.cs
+++ src/tools/ECMemoryTable.cs
@@ -6,6 +6,7 @@ public enum ECMemoryTable : UInt16
     EC_DATA_PORT = 0x4F,
     Fan1_RPM_Level = 0xC836,
     Fan2_RPM_Level = 0xC837,
-    Fan_RPM_SET = 0xC83D,
+    Fan1_RPM_SET = 0xC83C,
+    Fan2_RPM_SET = 0xC83D,
     EC_Version = 0xC411,
 }

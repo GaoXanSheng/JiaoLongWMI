dc4e16968898343fcf2ed5ee060d29d464bcd2dc
GaoXuSheng <moxiaojulove@foxmail.com>
2025-05-18 00:24:54 +0800

    feat: 添加一键构建脚本

---
+++ build.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop"
+
+dotnet clean
+
+dotnet publish -c Release -r win-x64 --self-contained true `
+  /p:PublishSingleFile=true `
+  /p:PublishReadyToRun=true `
+  /p:TargetFramework="net8.0-windows" `
+  /p:Platform="x64" `
+  /p:PublishDir="..\resources\"
+
+Write-Host "buildCompleted!"
